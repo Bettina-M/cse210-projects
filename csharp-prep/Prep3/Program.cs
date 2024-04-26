0101e799ac9ff8876e7a538ec270a1ad8de58bb1
unknown <betinalucher@gmail.com>
2024-04-26 16:46:39 +0300

    Finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Formats.Asn1;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random ();
+        int magicNumber = randomGenerator.Next(1,100);
+        
+        int guessNumber = -1;
+        
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+        
+
+            if (guessNumber<magicNumber)
+            {    
+                Console.WriteLine("Guess Higher");
+               
+                
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Guess Lower");
+                
+            }
+            else
+            {
+                Console.WriteLine("You Guessed It!");
+                              
+            }
+            
+        
+        }
+        
     }
 }
