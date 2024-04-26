de6b842fe49ef94e8b6b080db00945b06827d76c
unknown <betinalucher@gmail.com>
2024-04-26 16:56:28 +0300

    Finished Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+        DisplayResult();
+        
+        
+
+
     }
+        static void DisplayWelcome ()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        
+        static  string PromptUserName ()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+            
+        
+        }
+        
+        static int SquareNumber ( )
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            int SquareNumber = number * number ;
+            return SquareNumber;
+        }
+        static void DisplayResult ()
+        {
+            string UserName = PromptUserName();
+            int favNumber = SquareNumber();
+            
+             Console.WriteLine ($"{UserName},the square of your number is {favNumber}");
+            
+        }
+    
+    
 }
