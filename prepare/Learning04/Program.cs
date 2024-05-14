2fa6184b8885826058166e2c80ddb9d2da166248
Your Name <you@example.com>
2024-05-14 20:59:36 +0300

    Completed Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        
+        
+        
+        Assignment myobj1 = new Assignment("Bettina", "Algebra");
+
+        Console.WriteLine (myobj1.GetSummary());
+
+        MathAssignment myobj2 = new MathAssignment ("Bettina", "Algebra", "Section 7.3", "Problems 8-19");
+
+        Console.WriteLine(myobj2.GetSummary());
+        Console.WriteLine(myobj2.GetHomeWorkList());
+        
+        WritingAssignment myobj3 = new WritingAssignment ("Bettina", "African History", "Our beautiful continent");
+        
+        Console.WriteLine(myobj3.GetSummary());
+        Console.WriteLine(myobj3.GetWritingInformation());
+        
+        
+       
+
+        
     }
 }
